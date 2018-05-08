9fe9a3de72f5c43c6d1e80df8b3fe8a495b4e319
LAG <github.robot@lagtime.com>
2018-05-08 16:14:48 -0700

    Update and rename messaging.js to messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Messaging', message: 'Messaging functions' });
+  res.render('index', { title: 'Messages', message: 'Messaging functions' });
 });
 
 module.exports = router;
